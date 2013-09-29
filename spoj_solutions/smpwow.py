e89d86cb8fe810a9dceda9a013c9b3642004d865
Shantanu Sarkar <shantanusarkar.me@gmail.com>
2013-09-29 19:57:32 +0530

    SMPWOW solution

---
+++ spoj_solutions/smpwow.py
@@ -0,0 +1,8 @@
+## link to the question : http://www.spoj.com/BSCPROG/problems/SMPWOW/
+## Author : Shantanu Sarkar
+import sys
+a = int(raw_input())
+sys.stdout.write('W')
+for x in range(0, a):
+    sys.stdout.write('o')
+sys.stdout.write('w')
